408268d8c3126e62d82a50188bb0ac2af4ad1884
Khalil Najjar <knajjars@gmail.com>
2018-11-25 12:04:11 +0100

    changes

--- server/configs/database.js
+++ server/configs/database.js
@@ -1,13 +1,18 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // Don't forget to set "MONGODB_URI" in ~/server/.env
-const uri = process.env.MONGODB_URI || `mongodb://localhost/please-set-process-env-mongodb-uri`;
+const uri = process.env.MONGODB_URI || `mongodb://localhost/earth-chip`;
 
 mongoose
-  .connect(uri, { useNewUrlParser: true })
+  .connect(
+    uri,
+    { useNewUrlParser: true }
+  )
   .then(x => {
-    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
+    console.log(
+      `Connected to Mongo! Database name: "${x.connections[0].name}"`
+    );
   })
   .catch(err => {
-    console.error('Error connecting to mongo', err)
+    console.error("Error connecting to mongo", err);
   });
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const { isLoggedIn } = require('../middlewares')
+const express = require("express");
+const { isLoggedIn } = require("./middlewares");
 const router = express.Router();
 
-router.get('/secret', isLoggedIn, (req, res, next) => {
+router.get("/secret", isLoggedIn, (req, res, next) => {
   res.json({
     secret: 42,
     user: req.user
