1bdf17db673322c7f2c2524b619c3aa0fb0d36d7
tomjohnburton <tomjohnburton@users.noreply.github.com>
2018-11-26 15:56:58 +0100

    Nav added

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,16 +1,16 @@
 import React, { Component } from "react";
 import { Route, Link, NavLink, Switch } from "react-router-dom";
-import Home from "./pages/Home";
+
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import apiAuth from "../api/auth";
+import NavBar from "./pages/NavBar";
+import Home from "./pages/Home";
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      countries: []
-    };
+    this.state = {};
     // apiAuth.loadUser();
   }
 
@@ -21,25 +21,17 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">MERN Boilerplate</h1>
-          <NavLink to="/" exact>
-            Home
-          </NavLink>
-          {!apiAuth.isLoggedIn() && <NavLink to="/signup">Signup</NavLink>}
-          {!apiAuth.isLoggedIn() && <NavLink to="/login">Login</NavLink>}
-          {apiAuth.isLoggedIn() && (
-            <Link to="/" onClick={e => this.handleLogoutClick(e)}>
-              Logout
-            </Link>
-          )}
-        </header>
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/signup" component={Signup} />
-          <Route path="/login" component={Login} />
-          <Route render={() => <h2>404</h2>} />
-        </Switch>
+        <div className="nav">
+          <NavBar />
+        </div>
+        <div className="body">
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/signup" component={Signup} />
+            <Route path="/login" component={Login} />
+            <Route render={() => <h2>404</h2>} />
+          </Switch>
+        </div>
       </div>
     );
   }
