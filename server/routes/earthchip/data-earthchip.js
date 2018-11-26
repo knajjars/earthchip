c24d0ae1ed65fde1af36072fb316e4a775911187
Khalil Najjar <knajjars@gmail.com>
2018-11-26 09:05:35 +0100

    changed route to query

--- server/routes/earthchip/data-earthchip.js
+++ server/routes/earthchip/data-earthchip.js
@@ -5,32 +5,29 @@ const router = express.Router();
 
 // ALL ROUTES PREFIXED WITH /api/data-earthchip
 
-router.get(
-  "/:macAddress/:environmentHumidity/:environmentTemp/:soilMoisture",
-  (req, res, next) => {
-    if (
-      !req.params.macAddress ||
-      !req.params.environmentHumidity ||
-      !req.params.environmentTemp ||
-      !req.params.soilMoisture
-    ) {
-      res.status(500).json({ message: "Information is incomplete!" });
-      return;
-    }
-
-    const data = ({
-      macAddress,
-      environmentTemp,
-      environmentHumidity,
-      soilMoisture
-    } = req.params);
-
-    EarthChipData.create(data)
-      .then(data => {
-        res.json(data);
-      })
-      .catch(err => next(err));
+router.get("/", (req, res, next) => {
+  if (
+    !req.query.macAddress ||
+    !req.query.environmentHumidity ||
+    !req.query.environmentTemp ||
+    !req.query.soilMoisture
+  ) {
+    res.status(500).json({ message: "Information is incomplete!" });
+    return;
   }
-);
+
+  const data = ({
+    macAddress,
+    environmentTemp,
+    environmentHumidity,
+    soilMoisture
+  } = req.query);
+
+  EarthChipData.create(data)
+    .then(data => {
+      res.json(data);
+    })
+    .catch(err => next(err));
+});
 
 module.exports = router;
