0ad041f92541a188f9841069a25019f5fe0ad681
tomjohnburton <tomjohnburton@users.noreply.github.com>
2018-11-27 09:16:44 +0100

    homepage css

--- client/src/components/pages/Home.jsx
+++ client/src/components/pages/Home.jsx
@@ -1,5 +1,31 @@
 import React from "react";
+import { Button } from "antd";
 
 export default function Home() {
-  return <div>HELLO</div>;
+  return (
+    <div className="home-page">
+      <div className="commands">
+        <div className="title">
+          <h1>EARTHCHI P</h1>
+        </div>
+        <div className="buttons">
+          {/* <Button type="default" size="large" ghost>
+            Sign Up
+          </Button>
+          <Button type="default" size="large" ghost>
+            Log In
+          </Button> */}
+        </div>
+      </div>
+
+      <div className="logo">
+        <div>
+          <span class="pulse" />
+        </div>
+        <div>
+          <img src="./images/micro_temp.png" alt="chip png" />
+        </div>
+      </div>
+    </div>
+  );
 }
